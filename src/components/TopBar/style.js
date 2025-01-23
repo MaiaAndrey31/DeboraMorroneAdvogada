759e0af23f58c2b3050c829359b14d31048de257
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-01-23 08:41:34 -0300

    adjustTopBar

--- src/components/TopBar/style.js
+++ src/components/TopBar/style.js
@@ -60,7 +60,7 @@ gap: 0.40rem;
 
 a {
     display: flex;
-    flex-direction: column-reverse;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     gap: 0.40rem;
